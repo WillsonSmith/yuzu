958c5b1955bfd81d05a9f3b7c54eaf7df6b17725
Willson Smith <me@willsonsmith.com>
2017-08-22 21:18:40 -0400

    some fake canvas stuff

--- src/js/stars/StarSheet.spec.ts
+++ src/js/stars/StarSheet.spec.ts
@@ -5,29 +5,32 @@ import StarSheet from './StarSheet';
 
 describe('StarSheet', () => {
   let Stars = null;
-  let window = null;
+  let fakeCanvas = null;
 
   beforeEach((done) => {
-    const fakeCanvas = {
-      width: 300,
-      height: 150,
-      style: {
-        width: '300px',
-        height: '150px',
-      },
-      getContext() {
-        return {
-          fillStyle: '',
-          scale(scaleX, scaleY) {},
-          beginPath() {},
-          arc() {},
-          fill() {},
+
+    fakeCanvas = function() {
+      return {
+        get offsetWidth() { return parseInt(this.style.width, 10) },
+        get offsetHeight() { return parseInt(this.style.height, 10) },
+        style: {
+          width: '300px',
+          height: '150px',
+        },
+        getContext() {
+          return {
+            fillStyle: '',
+            scale(scaleX, scaleY) {},
+            beginPath() {},
+            arc() {},
+            fill() {},
+          }
         }
       }
-    };
+    }
 
     Stars = new StarSheet({
-      canvas:fakeCanvas as any,
+      canvas:fakeCanvas() as any,
       numberOfStars: 100,
       startSeed: 10,
       height: 500,
@@ -39,8 +42,10 @@ describe('StarSheet', () => {
   });
 
   describe('constructor', () => {
-    it('assigns the seed, rng, canvas, width, height, and context to the instance', () => {
-      assert.equal(10, Stars.seed);
+    it('assigns values on construction', () => {
+      assert.equal(Stars.seed, 10);
+      assert.equal(Stars.width, 500)
+      assert.equal(Stars.height, 500)
     });
   });
 });
