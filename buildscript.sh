a6fc1fbec1612942445a3de784a4fb1e001b920d
Willson Smith <willson.smith@shopify.com>
2019-03-01 13:31:02 -0500

    move to parcel

--- buildscript.sh
+++
@@ -1,6 +0,0 @@
-postcss src/css/styles.css --output build/css/styles.c.css;
-webpack --config webpack.config.build.js --optimize-minimize;
-cp -R src/js/vendor build/js/vendor;
-cp -R src/fonts build/fonts;
-cp -R src/images build/images;
-cp src/index.html build/index.html;
--- src/js/app.ts
+++ src/js/app.ts
@@ -1,24 +1,24 @@
 import StarSheet from './stars/StarSheet';
 
-interface fontFaceOptions {
-    tolerance?: number;
-    delay?: number;
-    glyphs?: string;
-    success?: Function;
-    error?: Function;
-    timeout?: number;
-    weight?: string;
-    style?: string;
-}
+// interface fontFaceOptions {
+//     tolerance?: number;
+//     delay?: number;
+//     glyphs?: string;
+//     success?: Function;
+//     error?: Function;
+//     timeout?: number;
+//     weight?: string;
+//     style?: string;
+// }
 
-declare function FontFaceOnload(FontFamily:string, options:fontFaceOptions)
-FontFaceOnload('Toronto Subway', {
-  success: () => {
-    [].slice.call(document.querySelectorAll('.top-billing')).forEach((node) => node.classList.add('top-billing--fonts-loaded'));
-  },
-  error: function() {},
-  timeout: 5000 // in ms. Optional, default is 10 seconds
-});
+// declare function FontFaceOnload(FontFamily:string, options:fontFaceOptions)
+// FontFaceOnload('Toronto Subway', {
+//   success: () => {
+//     [].slice.call(document.querySelectorAll('.top-billing')).forEach((node) => node.classList.add('top-billing--fonts-loaded'));
+//   },
+//   error: function() {},
+//   timeout: 5000 // in ms. Optional, default is 10 seconds
+// });
 
 const canvas = document.getElementById('stars');
 const startSeed = Date.now();
