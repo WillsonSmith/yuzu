d03255be0b39528cb8c066e6e10d44479cbef577
Willson Smith <me@willsonsmith.com>
2022-05-02 22:08:35 -0400

    scaffold

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,19 @@
  * Docs: https://developers.google.com/web/ilt/pwa/introduction-to-service-worker
  */
 
+const filesToCache = [
+  '/',
+  'index.html',
+  'assets/css/main.css',
+  'assets/js/index.js',
+  'assets/static/icon.png',
+  '/assets/vendor/shoelace/dist/themes/light.css',
+  '/assets/vendor/shoelace/dist/themes/dark.css',
+  /**
+   * Add caching for each imported shoelace component
+   */
+];
+
 self.addEventListener('install', (event) => {
   // Perform install steps
 });
