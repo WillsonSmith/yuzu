3b8a80a0cfa5d97d910e8002859a525d92bb34e3
Willson Smith <me@willsonsmith.com>
2022-05-08 11:55:54 -0400

    add basics to index for using shoelace components

--- assets/js/(._.).js
+++ assets/js/(._.).js
@@ -2,5 +2,8 @@
 import { setBasePath } from './components/index.js';
 setBasePath('/assets/vendor/shoelace/dist');
 
+/** Shoelace components used on index */
+import '@shoelace-style/shoelace/dist/components/card/card.js';
+
 /** Main page required components components */
 import './components/page-header/page-header.js';
