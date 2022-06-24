2aba08b62a51c2cf314791fe3ee4d7130910b1a1
Willson Smith <me@willsonsmith.com>
2022-06-24 18:08:52 -0400

    Update spacing values and use spacing values for block card

--- shared/web-components/_system/tokens/spacing.js
+++ shared/web-components/_system/tokens/spacing.js
@@ -4,9 +4,9 @@ export const spacing = css`
   :host {
     /* --yz-spacing-01: 4px; */
     --yz-spacing-01: 0.25rem;
-    --yz-spacing-02: calc(var(--yz-spacing-base) * 2);
-    --yz-spacing-03: calc(var(--yz-spacing-base) * 3);
-    --yz-spacing-04: calc(var(--yz-spacing-base) * 4);
-    --yz-spacing-05: calc(var(--yz-spacing-base) * 5);
+    --yz-spacing-02: calc(var(--yz-spacing-01) * 2);
+    --yz-spacing-03: calc(var(--yz-spacing-01) * 3);
+    --yz-spacing-04: calc(var(--yz-spacing-01) * 4);
+    --yz-spacing-05: calc(var(--yz-spacing-01) * 5);
   }
 `;
--- shared/web-components/yz-block-card/yz-block-card.js
+++ shared/web-components/yz-block-card/yz-block-card.js
@@ -1,38 +1,37 @@
 import { LitElement, html, css } from "lit";
 import { styleMap } from "lit/directives/style-map.js";
 
+import { spacing } from "../_system/tokens/spacing.js";
+
 class YZBlockCard extends LitElement {
-  static get properties() {
-    return {
-      skew: { type: String },
-    };
-  }
-  static get styles() {
-    return css`
+  static properties = {
+    skew: { type: String },
+  };
+  static styles = [
+    spacing,
+    css`
       :host {
         display: block;
-        --yz-block-card-background: hsla(0, 0%, 100%, 1);
-        --yz-block-card-padding: calc(4px * 8);
-        --yz-shadow-width: 2;
-        --yz-shadow-color: hsla(0, 0%, 0%, 1);
+        --block-card-background: hsla(0, 0%, 100%, 1);
+        --block-card-padding: var(--yz-spacing-05);
+        --shadow-color: hsla(0, 0%, 0%, 1);
+        --shadow-width: 2;
         --skew: 0;
       }
-      .yz-block-card {
-        background: var(--yz-block-card-background);
-        box-shadow: 2px 2px 0 2px var(--yz-shadow-color),
-          inset 2px 2px 0 2px var(--yz-shadow-color);
+      .block-card {
+        background: var(--block-card-background);
+        box-shadow: 2px 2px 0 2px var(--shadow-color),
+          inset 2px 2px 0 2px var(--shadow-color);
 
-        padding: var(--yz-block-card-padding);
+        padding: var(--block-card-padding);
         transform: skew(var(--skew));
       }
-
-      .yz-block-card-content {
+      .block-card-content {
         transform: skew(calc(var(--skew) * -1));
       }
-    `;
-  }
+    `,
+  ];
 
-  skew = ``;
   render() {
     let styles = {};
     if (this.skew) {
@@ -41,8 +40,8 @@ class YZBlockCard extends LitElement {
       });
     }
     return html`
-      <div class="yz-block-card" style=${styles}>
-        <div class="yz-block-card-content">
+      <div class="block-card" style=${styles}>
+        <div class="block-card-content">
           <slot></slot>
         </div>
       </div>
