9bb0756cf415d4d076e79b188a241b73511c6f25
Willson Smith <me@willsonsmith.com>
2022-07-02 18:37:35 -0400

    can now pass a grid value or a number

--- shared/web-components/yz-grid/yz-grid.js
+++ shared/web-components/yz-grid/yz-grid.js
@@ -6,18 +6,30 @@ import { spacing } from "../_system/tokens/spacing.js";
 
 class Grid extends LitElement {
   static properties = {
-    rows: { type: Number },
-    columns: { type: Number },
+    rows: {},
+    columns: {},
     gap: { type: Number },
   };
   static styles = [
     spacing,
     css`
       :host {
+        --grid-template-column-default: repeat(auto-fit, 1fr);
+        --grid-template-row-default: repeat(auto-fit, 1fr);
+        --grid-gap-default: var(--yz-spacing-00);
         display: block;
       }
       .grid {
         display: grid;
+        grid-template-columns: var(
+          --grid-template-columns,
+          --grid-template-column-default
+        );
+        grid-template-rows: var(
+          --grid-template-rows,
+          --grid-template-row-default
+        );
+        grid-gap: var(--grid-gap, --grid-gap-default);
       }
     `,
   ];
@@ -30,16 +42,23 @@ class Grid extends LitElement {
   }
 
   render() {
+    let columns;
+    if (this.columns && isNaN(Number(this.columns))) columns = this.columns;
+    if (this.columns && !isNaN(Number(this.columns)))
+      columns = `repeat(${this.columns}, 1fr)`;
+
+    let rows;
+    if (this.rows && isNaN(Number(this.rows))) rows = this.rows;
+    if (this.rows && !isNaN(Number(this.rows)))
+      rows = `repeat(${this.rows}, 1fr)`;
+
+    const styles = {
+      "--grid-template-columns": columns,
+      "--grid-template-rows": rows,
+      "--grid-gap": `var(--yz-spacing-0${this.gap})`,
+    };
     return html`
-      <div
-        part="container"
-        class="grid"
-        style=${styleMap({
-          gridTemplateColumns: `repeat(${this.columns}, 1fr)`,
-          gridTemplateRows: `repeat(${this.rows}, 1fr)`,
-          gridGap: `var(--yz-spacing-0${this.gap});`,
-        })}
-      >
+      <div part="container" class="grid" style=${styleMap(styles)}>
         <slot></slot>
       </div>
     `;
